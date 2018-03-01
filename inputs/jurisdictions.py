f82aefe5c43ed98c0e0b23dc5143ff0489c758ed
sarangof <sarangofc@gmail.com>
2018-03-01 12:58:05 -0500

    Uniform names for cities and counties.

---
+++ inputs/jurisdictions.py
@@ -0,0 +1,122 @@
+juris_dict = {
+
+    'rockland_ny': {'short': None,
+                    'coords':[-74.269744, 40.995225, -73.892241, 41.326738],
+                    'handles': []}, 
+
+    'el_paso_tx': {'short': None,
+                    'coords': [-106.653179, 31.611867, -106.19244, 32.004576], 
+                    'handles': []},
+
+    'bernalillo_nm': {'short': None,
+                        'coords': [-106.585276, 35.28749, -106.517601, 35.337001], 
+                        'handles': []}, 
+
+    'lucas_oh': {'short': None,
+                  'coords':  [-83.893994, 41.406872, -83.178509, 41.739738], 
+                  'handles': []},
+
+    'redlands_ca': {'short': None,
+                    'coords': [-117.25708, 34.108485, -117.036929, 33.966542], 
+                    'handles': []}, 
+                    
+    'nashville_TN': {'short': None,
+                    'coords': [-87.045726, 35.963951, -86.518172, 36.388082], 
+                    'handles': []}, 
+
+    'las_vegas_nv': {'short': None,
+                    'coords': [-115.447428, 36.01523, -115.006297, 36.384162],
+                    'handles': []}, 
+      
+    'duval_fl': {'short': None,
+                    'coords': [-82.076054, 30.090064, -81.324093, 30.621692], 
+                    'handles': []}, 
+
+    'los_angeles_ca': {'short': 'LA',
+                    'coords': [-118.694684, 33.558664, -117.858083, 34.35037], 
+                    'handles': []}, 
+    'kenton_ky': 
+                    {'short': None,
+                    'coords': [-84.63384, 38.802213, -84.392141, 39.097515],  
+                    'handles': []}, 
+    'baton_rouge_la':
+                    {'short': None,
+                    'coords': [-91.320497, 30.291573, -90.829456, 30.734743], 
+                    'handles': []}, 
+    'boston_ma': 
+                    {'short': None,
+                    'coords': [-71.198945, 42.26042, -70.966859, 42.405593], 
+                    'handles': []}, 
+    'genesee_mi': 
+                    {'short': None,
+                    'coords': [-83.699419, 43.045778, -83.573077, 43.138292], 
+                    'handles': []}, 
+    'detroit_MI': 
+                    {'short': None,
+                    'coords': [-83.400165, 42.222532, -82.873412, 42.570363], 
+                    'handles': []}, 
+    'winnebago_wi': 
+                    {'short': None,
+                    'coords': [-88.901814, 43.880343, -88.397991, 44.255881],  
+                    'handles': []}, 
+    'thurston_wa': 
+                    {'short': None,
+                    'coords': [-123.234222, 46.762798, -122.14806, 47.189895],  
+                    'handles': []}, 
+    'baltimore_md': 
+                    {'short': None,
+                    'coords': [-76.770649, 39.190476, -76.487698, 39.414438], 
+                    'handles': []}, 
+    'douglas_co': 
+                    {'short': None,
+                    'coords': [-105.309384, 39.109217, -104.647458, 39.573343], 
+                    'handles': []}, 
+    'houston_tx': 
+                    {'short': None,
+                    'coords': [-95.813315, 29.52693, -95.037406, 30.133315], 
+                    'handles': []}, 
+    'san_luis_obispo_ca': 
+                    {'short': None,
+                    'coords': [-120.726177, 35.237026, -120.616828, 35.313684], 
+                    'handles': []}, 
+    'jackson_mo': 
+                    {'short': None,
+                    'coords': [-94.643005, 38.824799, -94.110168, 39.238704], 
+                    'handles': []}, 
+    'washington_dc': 
+                    {'short': None,
+                    'coords': [-77.11539, 38.830883, -76.894947, 38.994653],
+                    'handles': []},  
+    'chicago_il': 
+                    {'short': None,
+                    'coords': [-87.971568, 41.630378, -87.490656, 42.047215], 
+                    'handles': []}, 
+    'bexar_tx': 
+                    {'short': None,
+                    'coords': [-98.832552, 29.123447, -98.101961, 29.752529], 
+                    'handles': []}, 
+    'santa_clara_ca': 
+                    {'short': None,
+                    'coords': [-122.027937, 37.31364, -121.922101, 37.424878], 
+                    'handles': []}, 
+    'new_york_ny': 
+                    {'short': 'NY',
+                    'coords': [-74.257118, 40.485452, -73.700254, 40.911309], 
+                    'handles': ['nypd']}, 
+    'erie_ny': 
+                    {'short': None,
+                    'coords': [-79.156287, 42.47089, -78.424323, 43.091765], 
+                    'handles': []}, 
+    'seattle_wa': 
+                    {'short': None,
+                    'coords': [-122.445623, 47.491494, -122.226861, 47.737144], 
+                    'handles': []}, 
+    'eagan_city_mn': 
+                    {'short': None,
+                    'coords': [-93.236549, 44.774482, -93.101403, 44.863883], 
+                    'handles': []}, 
+    'richland_sc': 
+                    {'short': None,
+                    'coords': [-81.389102, 33.75143, -80.568427, 34.260967],
+                    'handles': []}
+}
